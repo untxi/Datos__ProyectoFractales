c65af67b355293a2c44147667a64875bc676ea73
marsep27 <marsep27@hotmail.com>
2014-09-25 15:44:43 -0600

    Levy lIsto!!!

--- ProyectoFractales/include/CurvaLevy.h
+++ ProyectoFractales/include/CurvaLevy.h
@@ -9,37 +9,31 @@ using namespace std;
 LinkedList<char> CurvaLevy(int pIteracion){
     // Atributos
     LinkedList <char> miListaItera;
-    int posActual;
+    if (pIteracion != 0){miListaItera.append('A');}
     //int sizeLista;
     // Si esta vacía, inicia en D
-        for (int i = 0; i <= pIteracion; i++){
+        for (int i = 0; i < pIteracion; i++){
             int sizeMiLista = miListaItera.getSize();
             miListaItera.goToStart();
-            for (int pos = 0; pos < sizeMiLista; pos++){
+            for (int pos = 0; pos < sizeMiLista; pos++)
+            {
                 miListaItera.goToPos(pos);
                 char elemento = miListaItera.getElement();
-                //std::cout << elemento;
-                if (elemento == 'A'){
-                    int posActual = miListaItera.getPos();
+                if (elemento == 'A')
+                {
+                    int posActual  = miListaItera.getPos();
                     miListaItera.insert('D');
                     miListaItera.insert('A');
                     miListaItera.insert('I');
                     miListaItera.insert('I');
                     miListaItera.insert('A');
                     miListaItera.insert('D');
-                    posActual += 6;
+                    posActual = pos = posActual + 6;
                     miListaItera.goToPos(posActual);
                     miListaItera.remove();
+                    sizeMiLista = miListaItera.getSize();
                 }
             }
         }
-    int sL = miListaItera.getSize();
-    for (int i = 0; i < sL; i++)
-    {
-        miListaItera.goToPos(i);
-        char elemL = miListaItera.getElement();
-        std::cout << "-" << elemL ;
-
-    }
     return miListaItera;
 };
--- ProyectoFractales/include/DibujaLevy.h
+++ ProyectoFractales/include/DibujaLevy.h
@@ -1,1 +1,14 @@
-/** ~~~ Dibuja Levy ~~~ */
+#ifndef DIBUJALEVY_H
+#define DIBUJALEVY_H
+
+
+class DibujaLevy
+{
+    public:
+        DibujaLevy();
+        virtual ~DibujaLevy();
+    protected:
+    private:
+};
+
+#endif // DIBUJALEVY_H
